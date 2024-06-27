547dbde9550f65d7799f772cd675b626e92012dc
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-26 21:38:58 -0700

    Fixed code

--- models/city.py
+++ models/city.py
@@ -14,8 +14,16 @@ class City(BaseModel, Base):
         state_id: The state id
         name: input name
     """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-#    places = relationship("Place", cascade='all, delete, delete-orphan',
-#                          backref="cities")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", cascade="all, delete-orphan",
+                          backref="cities")
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -16,8 +16,15 @@ class User(BaseModel, Base):
         _password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = relationship('Place',
+                              backref="user",
+                              cascade="all, delete-orphan")
     else:
         email = ""
         _password = ""
         first_name = ""
         last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
