17bc7bf7e89209615c93a4f744efd447c7dce90d
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-26 22:00:04 -0700

    Fixed code

--- models/review.py
+++ models/review.py
@@ -5,6 +5,21 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        text = Column(String(1024),
+                      nullable=False)
+        place_id = Column(String(60),
+                          ForeignKey('places.id'),
+                          nullable=False)
+        user_id = Column(String(60),
+                         ForeignKey('users.id'),
+                         nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
