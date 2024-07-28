00f27a96111396710e067cc67287e777ec31d072
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-28 18:26:31 +0100

    Fixed code

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -62,6 +62,10 @@ def number_odd_or_even(n):
     Number Odd or Even template route for pages
     """
     odd_or_even = "even" if n % 2 == 0 else "odd"
-    return render_template("6-number_odd_or_even.html", n=n, odd_or_even=odd_or_even)
+    return render_template(
+        "6-number_odd_or_even.html", n=n, odd_or_even=odd_or_even
+    )
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
