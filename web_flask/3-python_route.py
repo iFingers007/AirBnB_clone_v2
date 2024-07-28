697132aab22cecfa814110dceec2c6207a489b82
Abu Jehoiada <abujehoiada@gmail.com>
2024-07-28 16:20:42 +0100

    Fixed code

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,5 +23,22 @@ def hbnb():
     return "HBNB"
 
 
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    Replaces Underscore with text of the argument
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    Replaces Underscore with text of the argument
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
